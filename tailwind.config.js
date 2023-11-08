6ef0d9e83fc711cb962ce36d851cbf6794a9e523
saladpag69 <saladpag@gmail.com>
2023-11-09 00:26:18 +0700

    savedata

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwi
+ * ndcss').Config} */
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
